d54aa1df2dd9e6bcb851082615af829f30f08705
Darryl Hamilton <darrylh@catalyst.net.nz>
2013-01-11 11:59:59 +1300

    Extend cricinfo_link helper to include more object types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def active_page?(page)
     "active" if page == params[:controller]
   end
+
+  def cricinfo_link(object)
+    case object
+      when Match then link_to('CricInfo page', "http://www.espncricinfo.com/ci/engine/match/#{object.cricinfo_id}.html")
+      when Inning then link_to('CricInfo page', "http://www.espncricinfo.com/ci/engine/match/#{object.match.cricinfo_id}.html")
+      when Player then link_to('CricInfo page', "http://www.espncricinfo.com/ci/content/player/#{object.cricinfo_id}.html")
+      else 'hello'
+    end
+  end
 end
