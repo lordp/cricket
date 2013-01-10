3d9f600c5cf7a3c179b64fd28447fc704908cee4
Darryl Hamilton <darrylh@catalyst.net.nz>
2013-01-11 12:01:07 +1300

    Remove cricinfo helper and add victim count helper

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,7 +1,24 @@
 module PlayersHelper
 
-  def cricinfo_link(player)
-    link_to('CricInfo page', "http://www.espncricinfo.com/ci/content/player/#{player.cricinfo_id}.html")
+  def victim_count(type, innings)
+    bowlers = fielders = {}
+    innings.each do |inning|
+      if bowler = inning.fielders.find_by_involvement(Fielder::BOWLER)
+        bowlers[bowler.player_id]   ||= 0
+        bowlers[bowler.player_id]   += 1
+      end
+
+      if fielder = inning.fielders.find_by_involvement(Fielder::FIELDER)
+        fielders[fielder.player_id] ||= 0
+        fielders[fielder.player_id] += 1
+      end
+    end
+
+    case type
+      when 'bowler' then bowlers.sort_by { |k,v| v }.last
+      when 'fielder' then fielders.sort_by { |k,v| v }.last
+      else {}
+    end
   end
 
 end
