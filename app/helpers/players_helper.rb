2e308cc5f1a87b1a4d41adcf44acd4d093724d44
Darryl Hamilton <darrylh@catalyst.net.nz>
2013-01-11 13:10:45 +1300

    Improve the victim/antagonist methods

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,24 +1,12 @@
 module PlayersHelper
 
-  def victim_count(type, innings)
-    bowlers = fielders = {}
-    innings.each do |inning|
-      if bowler = inning.fielders.find_by_involvement(Fielder::BOWLER)
-        bowlers[bowler.player_id]   ||= 0
-        bowlers[bowler.player_id]   += 1
-      end
-
-      if fielder = inning.fielders.find_by_involvement(Fielder::FIELDER)
-        fielders[fielder.player_id] ||= 0
-        fielders[fielder.player_id] += 1
-      end
+  def antagonist_count(antagonists)
+    links = ''
+    antagonists.each do |antagonist|
+      player = Player.find(antagonist[0])
+      links += content_tag('li', link_to("#{player.name} (#{pluralize(antagonist[1], 'time')})", player), {}, false)
     end
 
-    case type
-      when 'bowler' then bowlers.sort_by { |k,v| v }.last
-      when 'fielder' then fielders.sort_by { |k,v| v }.last
-      else {}
-    end
+    content_tag('ol', links, {}, false)
   end
-
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -6,4 +6,11 @@ class Player < ActiveRecord::Base
   extend FriendlyId
   friendly_id :full_name, :use => :slugged
 
+  def victim_count(involvement)
+    Fielder.joins(:inning).select('innings.player_id').where('fielders.player_id = ?', self.id).where('fielders.involvement = ?', involvement).group('innings.player_id').limit(5).count(:order => 'COUNT(innings.player_id) DESC')
+  end
+
+  def victim_count_as_batter(involvement)
+    Inning.joins(:fielders).select('fielders.player_id').where('innings.player_id = ?', self.id).where('fielders.involvement = ?', involvement).group('fielders.player_id').limit(5).count(:order => 'COUNT(fielders.player_id) DESC')
+  end
 end
